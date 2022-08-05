cf6053411ed7f79956eddf4e9e1243aafe31e4ad
kiddlu <kidd.dawny.lu@gmail.com>
2022-08-05 14:38:01 +0800

    update
    Signed-off-by: kiddlu <kidd.dawny.lu@gmail.com>

---
+++ memcpy/neon/neon/compress.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <sys/stat.h>
+
+#include "header.h"
+
+static void neon_memcpy(volatile unsigned char *dst, volatile unsigned char *src, int sz){
+    if (sz & 63)
+        sz = (sz & -64) + 64;
+    asm volatile (
+    "NEONCopyPLD: \n"
+            " VLDM %[src]!,{d0-d7} \n"
+            " VSTM %[dst]!,{d0-d7} \n"
+            " SUBS %[sz],%[sz],#0x40 \n"
+            " BGT NEONCopyPLD \n"
+    : [dst]"+r"(dst), [src]"+r"(src), [sz]"+r"(sz) : : "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "cc", "memory");
+}
+
+unsigned int get_file_size(FILE *fp)
+{
+   unsigned int length;
+#if 1
+    fseek(fp, 0L, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+#else
+    struct stat st;
+    fstat(fileno(fp), &st);
+    length = st.st_size;
+#endif
+    return length;
+}
+
+void usage(void)
+{
+    printf("useage: exe in out\n");
+}
+
+void memdump(void *addr, uint32_t size)
+{
+    int index;
+
+    while (size > 0) {
+        fprintf(stdout, "%p: ", addr);
+        index = 0;
+
+        // 32 bytes in the line
+        while (index < 32) {
+            fprintf(stdout, "%02x ", *((unsigned char *)addr));
+
+            index++;
+            addr++;
+            size--;
+
+            if (size <= 0)
+                break;
+        }
+        fprintf(stdout, "\n");
+    }
+    fprintf(stdout, "\n");
+    return;
+}
+
+void print_header_info(struct depth_header *h)
+{
+    if(h == NULL){
+        return;
+        printf("header null\n");
+    } else if(h->magic_num != PACKAGE_HEADER_MAGIC_NUM) {
+        printf("h->magic_num 0x%08x error\n", h->magic_num);
+        return;
+    }
+
+    printf("content_size: %d Bytes\n", h->content_size);
+    printf("origin_size : %d Bytes\n", h->origin_size);
+    
+    printf("content_format:  0x%02x\n", h->content_format);
+    printf("content_csum  :  0x%02x\n", h->content_csum);
+    printf("origin_csum   :  0x%02x\n", h->origin_csum);
+    printf("header_ver    :  0x%02x\n", h->header_ver);
+
+}
+
+
+int main(int argc, char* argv[])
+{
+    if(argc != 3) {
+        usage();
+        return -1;
+    } else if ( 0 != access(argv[1], F_OK)) {
+        usage();
+        return -1;
+    }
+
+    FILE *fp = fopen(argv[1], "rb");
+    uint32_t len = get_file_size(fp);
+
+    char *data = malloc(len);
+    fread(data, len, 1, fp);
+
+#if 0
+    printf("\n");
+    printf("hexdump of file %s (%d bytes):\n", argv[1], len);
+
+    memdump(data, len);
+#endif
+
+    uint32_t olen;
+    char *output = malloc(len);
+	
+	for(int i=0;i<3000;i++)
+    neon_memcpy(output, data, len);
+    olen = len;
+
+	struct depth_header *header = malloc(PACKAGE_HEADER_SIZE);
+    header->magic_num =  PACKAGE_HEADER_MAGIC_NUM;
+    header->content_size = olen;
+    header->origin_size  = len;
+    header->content_format = PACKAGE_CONTENT_FORMAT_DATA;
+    header->content_csum = header_csum(output, olen);
+    header->origin_csum  = header_csum(data, len);
+    header->header_ver = PACKAGE_HEADER_VERSION;
+    print_header_info(header);
+
+    fp = fopen(argv[2], "wb+");
+    fwrite(header, PACKAGE_HEADER_SIZE, 1, fp);
+    fwrite(output, olen, 1, fp);
+    fclose(fp);
+
+    return 0;
+}
+
---
+++ memcpy/neon/sse2neon/neon_memcpy.c
@@ -0,0 +1,80 @@
+// Source
+// https://github.com/skywind3000/FastMemcpy.git
+// https://github.com/anholt/mesa.git
+// https://github.com/DLTcollab/sse2neon.git
+
+#include <stdio.h>
+#include <string.h>
+#include "sse2neon.h"
+
+void mesa__memcpy(void *restrict dst, void *restrict src, size_t len)
+{
+   char *restrict d = dst;
+   char *restrict s = src;
+   size_t padding;
+   int i;
+
+
+   if (((uintptr_t)d & 15) != ((uintptr_t)s & 15)) {
+      
+      padding = (16 - (((size_t)dst) & 15)) & 15;
+
+      if (padding > 0) {
+         __m128i head = _mm_loadu_si128((const __m128i*)s);
+         _mm_storeu_si128((__m128i*)d, head);
+         dst += padding;
+         src += padding;
+         len -= padding;
+      }
+
+
+      
+      for(i=len;i>=64;i=i-64){
+         __m128i *dst_cacheline = (__m128i *)d;
+         __m128i *src_cacheline = (__m128i *)s;
+
+
+         __m128i m0 = _mm_loadu_si128(((const __m128i*)src_cacheline) + 0);
+         __m128i m1 = _mm_loadu_si128(((const __m128i*)src_cacheline) + 1);
+         __m128i m2 = _mm_loadu_si128(((const __m128i*)src_cacheline) + 2);
+         __m128i m3 = _mm_loadu_si128(((const __m128i*)src_cacheline) + 3);
+         _mm_storeu_si128(((__m128i*)dst_cacheline) + 0, m0);
+         _mm_storeu_si128(((__m128i*)dst_cacheline) + 1, m1);
+         _mm_storeu_si128(((__m128i*)dst_cacheline) + 2, m2);
+         _mm_storeu_si128(((__m128i*)dst_cacheline) + 3, m3);
+
+         _mm_prefetch((const char*)(src_cacheline + 64), _MM_HINT_NTA);
+
+         d += 64;
+         s += 64;
+         //len -= 64;
+      }
+   }
+
+
+   for(i=len;i>=64;i=i-64){
+      __m128i *dst_cacheline = (__m128i *)d;
+      __m128i *src_cacheline = (__m128i *)s;
+
+   __m128i temp1 = _mm_stream_load_si128(src_cacheline + 0);
+   __m128i temp2 = _mm_stream_load_si128(src_cacheline + 1);
+   __m128i temp3 = _mm_stream_load_si128(src_cacheline + 2);
+   __m128i temp4 = _mm_stream_load_si128(src_cacheline + 3);
+
+   _mm_store_si128(dst_cacheline + 0, temp1);
+   _mm_store_si128(dst_cacheline + 1, temp2);
+   _mm_store_si128(dst_cacheline + 2, temp3);
+   _mm_store_si128(dst_cacheline + 3, temp4);
+   _mm_prefetch((const char*)(src_cacheline + 64), _MM_HINT_NTA);
+      
+   d += 64;
+   s += 64;
+   //len -= 64;
+
+   }
+   len=i;
+   /* memcpy() the tail. */
+   if (len) {
+      memcpy(d, s, len);
+   }
+}
